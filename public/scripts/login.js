d135457a8af9fa193250899831addc8644594edb
Jayson1001 <64107977+Jayson1001@users.noreply.github.com>
2020-07-31 13:14:26 -0400

    login-page

--- public/scripts/login.js
+++ public/scripts/login.js
@@ -1,6 +1,45 @@
 let name = getElementbyId("name");
 let password = getElementbyId("password");
 
+
+firebase.auth().onAuthStateChanged(function(user) {
+    if (user) {
+      // User is signed in.
+      document.getElementById("user_div").style.display = "block";
+      document.getElementById("login_div").style.display = "none";
+
+    } else {
+      // No user is signed in.
+
+      document.getElementById("user_div").style.display = "none";
+      document.getElementById("login_div").style.display = "block";
+    }
+  });
+
+
 function login() {
-    if()
+    
+    var userEmail = document.getElementById("email_field").nodeValue;
+    var userPassword = document.getElementById("password_field");
+
+    window.alert("Error : " + errorMessage)
+    
+    firebase.auth().signInWithEmailAndPassword(userEmail, userPassword).catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // ...
+
+        
+      });
+
+    
+}
+
+function logout() {
+    firebase.auth().signOut().then(function() {
+        // Sign-out successful.
+      }).catch(function(error) {
+        // An error happened.
+      });
 }
