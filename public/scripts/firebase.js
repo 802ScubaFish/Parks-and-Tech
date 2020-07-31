10247e4be821acc9effdd00961228daa64d872db
Jayson1001 <64107977+Jayson1001@users.noreply.github.com>
2020-07-31 15:31:06 -0400

    login complete

--- public/scripts/firebase.js
+++ public/scripts/firebase.js
@@ -1,13 +1,13 @@
-// Utilizes firebase
-var firebase = require("firebase/app");
-
-// Add the Firebase products that you want to use
-require("firebase/auth");
-require("firebase/firestore");
-
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-firebase.analytics();
+const firebaseConfig = {
+  apiKey: "AIzaSyAR7Mp0IGpvHdJsy1VTJZwn3SBO_4txueU",
+  authDomain: "parksandtechvt.firebaseapp.com",
+  databaseURL: "https://parksandtechvt.firebaseio.com",
+  projectId: "parksandtechvt",
+  storageBucket: "parksandtechvt.appspot.com",
+  messagingSenderId: "75295634723",
+  appId: "1:75295634723:web:c34c863df326f7478ec896",
+  measurementId: "G-G29HTBFPZ9",
+};
 
 //Initializes the database
 const parksAndTechApp = firebase.initializeApp(firebaseConfig);
--- public/scripts/login.js
+++ public/scripts/login.js
@@ -1,8 +1,6 @@
-let name = getElementbyId("name");
-let password = getElementbyId("password");
 
 
-firebase.auth().onAuthStateChanged(function(user) {
+parksAndTechApp.auth().onAuthStateChanged(function(user) {
     if (user) {
       // User is signed in.
       document.getElementById("user_div").style.display = "block";
@@ -19,17 +17,17 @@ firebase.auth().onAuthStateChanged(function(user) {
 
 function login() {
     
-    var userEmail = document.getElementById("email_field").nodeValue;
-    var userPassword = document.getElementById("password_field");
+    var userEmail = document.getElementById("email_field").value;
+    var userPassword = document.getElementById("password_field").value;
 
-    window.alert("Error : " + errorMessage)
     
-    firebase.auth().signInWithEmailAndPassword(userEmail, userPassword).catch(function(error) {
+    parksAndTechApp.auth().signInWithEmailAndPassword(userEmail, userPassword).catch(function(error) {
         // Handle Errors here.
         var errorCode = error.code;
         var errorMessage = error.message;
         // ...
-
+        
+        window.alert("Error : " + errorMessage);
         
       });
 
@@ -37,7 +35,7 @@ function login() {
 }
 
 function logout() {
-    firebase.auth().signOut().then(function() {
+    parksAndTechApp.auth().signOut().then(function() {
         // Sign-out successful.
       }).catch(function(error) {
         // An error happened.
