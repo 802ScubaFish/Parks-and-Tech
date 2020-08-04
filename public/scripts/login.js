331dc84d9ab1fca98e9e046778178be7a952edaa
802ScubaFish <christianrdow@gmail.com>
2020-08-04 15:08:13 -0400

    added form display buttons

--- public/scripts/login.js
+++ public/scripts/login.js
@@ -1,42 +1,68 @@
+parksAndTechApp.auth().onAuthStateChanged(function (user) {
+  if (user) {
+    // User is signed in.
+    document.getElementById("userDiv").style.display = "flex";
+    document.getElementById("loginDiv").style.display = "none";
 
+  } else {
+    // No user is signed in.
 
-parksAndTechApp.auth().onAuthStateChanged(function(user) {
-    if (user) {
-      // User is signed in.
-      document.getElementById("userDiv").style.display = "block";
-      document.getElementById("loginDiv").style.display = "none";
-
-    } else {
-      // No user is signed in.
-
-      document.getElementById("userDiv").style.display = "none";
-      document.getElementById("loginDiv").style.display = "block";
-    }
-  });
+    document.getElementById("userDiv").style.display = "none";
+    document.getElementById("loginDiv").style.display = "block";
+  }
+});
 
 //Logs User In
 function login() {
-    
-    var userEmail = document.getElementById("emailField").value;
-    var userPassword = document.getElementById("passwordField").value;
 
-    
-    parksAndTechApp.auth().signInWithEmailAndPassword(userEmail, userPassword).catch(function(error) {
-        // Handle Errors here.
-        var errorCode = error.code;
-        var errorMessage = error.message;
-        // ...
-        
-        window.alert("Error : " + errorMessage);
-        
-      });
+  var userEmail = document.getElementById("emailField").value;
+  var userPassword = document.getElementById("passwordField").value;
+
+
+  parksAndTechApp.auth().signInWithEmailAndPassword(userEmail, userPassword).catch(function (error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // ...
+
+    window.alert("Error : " + errorMessage);
+
+  });
 
 }
 
 function logout() {
-    parksAndTechApp.auth().signOut().then(function() {
-        // Sign-out successful.
-      }).catch(function(error) {
-        // An error happened.
-      });
+  parksAndTechApp.auth().signOut().then(function () {
+    // Sign-out successful.
+  }).catch(function (error) {
+    // An error happened.
+  });
+}
+
+function displayEventForm() {
+
+  var eventForm = document.getElementById("wholeEventForm");
+
+  if (!eventForm.style.display) {
+    eventForm.style.display = "block";
+  } else if (eventForm.style.display === 'none') {
+    eventForm.style.display = "block";
+  } else {
+    eventForm.style.display = "none";
+  }
+
+}
+
+function displayFacilityForm() {
+
+  var facilityForm = document.getElementById("wholeFacilityForm");
+
+  if (!facilityForm.style.display) {
+    facilityForm.style.display = "block";
+  } else if (facilityForm.style.display === 'none') {
+    facilityForm.style.display = "block";
+  } else {
+    facilityForm.style.display = "none";
+  }
+
 }
