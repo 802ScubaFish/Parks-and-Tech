e716ca93085359244504b9935a8d1abea27b7059
Emily Reynolds <emilyreynoldsdsa@gmail.comgit config --global user.name Emily>
2020-08-05 11:07:23 -0400

    Added lat and longs

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -4,31 +4,39 @@ let map;
 
 // Creates the map and centers on Vermont
 function initMap() {
-    map = new google.maps.Map(document.getElementById("map"), {
-        center: {
-            lat: 44.5588,
-            lng: -72.5778
-        },
-        zoom: 7
-    });
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: {
+      lat: 44.5588,
+      lng: -72.5778,
+    },
+    zoom: 7,
+  });
 
-    myDb.ref("/Facilities").once("value", (res)=>{
-        console.log(res.val())
-    })
-    
+ 
+
+  myDb.ref("/Facilities").once("value", (res) => {
+    const object = res.val();
+    console.log(res.val())
+
+    for (const objectId in object) {
+      return(
+        `lat: ${object[objectId]["lat"]}, long: ${object[objectId]["long"]}`
+      );
+
+    }
+  });
 }
 
 //Hamburger Animation
 const menuButton = document.querySelector(".menuButton");
-let menuIsOpen =false;
-menuButton.addEventListener("click", ()=>{
-    console.log("This is clicked")
-    if (!menuIsOpen){
-        menuButton.classList.add("open");
-        menuIsOpen=true;
-        
-    }else{
-        menuButton.classList.remove("open");
-        menuIsOpen= false;
-    }
-})
+let menuIsOpen = false;
+menuButton.addEventListener("click", () => {
+  console.log("This is clicked");
+  if (!menuIsOpen) {
+    menuButton.classList.add("open");
+    menuIsOpen = true;
+  } else {
+    menuButton.classList.remove("open");
+    menuIsOpen = false;
+  }
+});
